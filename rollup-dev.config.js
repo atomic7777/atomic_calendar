2ca93cc8b2abbfa615aabedc2b388f90ede1fb5f
Victor Winberg <victor.m.winberg@gmail.com>
2020-04-09 08:35:12 +0200

    Fix errors and setup ngrok

--- rollup-dev.config.js
+++ rollup-dev.config.js
@@ -1,18 +1,25 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import serve from "rollup-plugin-serve";
 
+const serveopts = {
+  contentBase: ["./dist"],
+  host: "localhost",
+  port: 5000,
+  allowCrossOrigin: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+};
 
 export default {
-    input: 'app.js',
-    output: {
-        file: 'atomic_calendar.js',
-        format: 'iife'
-    },
-	watch: {
-    clearScreen: false
-    },
-    plugins: [
-        resolve(),
-	    commonjs({ fast: true }),
-    ]
+  input: "app.js",
+  output: {
+    file: "atomic_calendar.js",
+    format: "iife",
+  },
+  watch: {
+    clearScreen: false,
+  },
+  plugins: [resolve(), commonjs({ fast: true }), serve(serveopts)],
 };
